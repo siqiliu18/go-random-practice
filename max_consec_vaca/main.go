8f1944059ed23a8961977c1611f2157a73c8d6fd
Siqi Liu <siqiliu@Siqis-MacBook-Pro.local>
2023-12-06 11:02:51 -0800

    facebook 2nd question

---
+++ max_consec_vaca/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(`
+	// Maximize Longest Vacation
+	// [W, H, H, W, W, H, W]
+	// W: workday
+	// H: holiday
+	// p = 2 -> number of pto days
+	// result: 5
+	
+	// Input: array, p
+	// Output: maximum consecutive vacation days you can have
+	`)
+}
